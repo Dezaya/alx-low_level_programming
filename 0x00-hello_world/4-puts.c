96dd383355f1c4e7d1aab7fd696222587a199b2e
Dezaya <obidesire11@gmail.com>
2022-12-08 14:00:21 -0800

    Hello, Put...

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main -print the line of code using puts
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	puts("\"Programming is like a building a multilingual puzzle")
+		return(0)
+}
