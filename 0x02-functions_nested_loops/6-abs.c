db123fde34b4fc39efd55a0a45868ddc75e5b194
Dezaya <obidesire11@gmail.com>
2022-12-13 16:43:29 -0800

    Task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - print the absolute value of an integer
+ * @t: the interger to be printed
+ * return: the absolute value of int
+ */
+int _abs(int t)
+{
+	if (t >= 0)
+	{
+		return (t);
+	}
+	return (-t);
+}
